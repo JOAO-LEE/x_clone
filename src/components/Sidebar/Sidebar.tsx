6ad8a745dd339b5a20959ca141a885155913ebd2
João Lee <joaumlimaum@gmail.com>
2024-06-10 22:55:15 -0300

    Create first sidebar layout

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { XLogo, House } from "@phosphor-icons/react/dist/ssr";
+import Link from "next/link";
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <div className="flex flex-col gap-4 p-3">
+      <Link href="/">
+        <XLogo 
+        className="w-16 h-16 p-3 hover:bg-gray-100 rounded-full transition duration-200"
+        /> 
+      </Link>
+      <Link href="/" 
+      className="flex gap-2 items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 w-fit">
+        <House 
+        className="w-7 h-7" weight="fill"
+        />
+        <span 
+        className="font-bold hidden xl:inline">
+          Home
+        </span>
+      </Link>
+      <button className="bg-blue-400 text-white rounded-full hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline">Sign in</button>
+    </div>
   )
 }
 
